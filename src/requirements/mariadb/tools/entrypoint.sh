fc0363056161fe6e7708ed540cb489e744bb57d2
Clement Colin <clmnt.co@gmail.com>
2025-06-22 12:58:29 +0900

    ok

---
+++ src/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mysqld_safe --skip-networking &
+sleep 5
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql -u root <<-EOSQL
+		CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+		CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+		GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+		FLUSH PRIVILEGES;
+	EOSQL
+fi
+
+mysqladmin -u root -p"$MARIADB_ROOT_PASSWORD" shutdown
+
+exec mysqld --user=mysql
